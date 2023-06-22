cd9a69e0759f45bc3548929714db9ce3fbef40e8
Your Name <you@example.com>
2023-06-22 14:03:02 +0200

    proj

---
+++ api/controllers/auth.js
@@ -0,0 +1,17 @@
+export const register = (req,res) => {
+    
+    //CHECK USER IF EXIST
+    
+    //CREATE A NEW USER
+        //Hash the password
+    
+}
+
+export const login = (req,res) => {
+    
+}
+
+export const logout = (req,res) => {
+    
+}
+
