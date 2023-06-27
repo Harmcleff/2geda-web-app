4018de73b29abe2fffcc0bc51f801c98ae4631e2
Your Name <you@example.com>
2023-06-27 18:16:35 -0500

    prog

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom"
 import Register from './pages/Register';
 import Home from './pages/Home';
 import Login from './pages/Login';
-import './App.css'
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
