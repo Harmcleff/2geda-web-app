b055fe44e738caf4146ade289c475ef1cd05dc0b
Sanyaolu Hammed <33807817+Harmcleff@users.noreply.github.com>
2023-12-12 22:30:18 -0500

    Delete deployed directory

--- deployed/api/routes/comments.js
+++
@@ -1,9 +0,0 @@
-import Express from "express";
-import {getComments, addComment} from "../controllers/comment.js"
-
-const router = Express.Router()
-
-router.get("/",getComments)
-router.post("/",addComment)
-
-export default router
--- deployed/frontend/src/axios.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const makeRequest = axios.create({
-    baseURL: "https://2geda.tech/api/v1/",
-    withCredentials: true
-}); 
